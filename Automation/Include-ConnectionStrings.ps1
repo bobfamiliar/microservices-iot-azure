4480f4607813ecb2f4eb997b8a03f0cec913b3fd
bob familiar <bob.familiar@outlook.com>
2015-10-20 17:57:49 -0400

    Cleaning the repo for release

--- Automation/Include-ConnectionStrings.ps1
+++ Automation/Include-ConnectionStrings.ps1
@@ -1,4 +1,4 @@
-$docdburi = "https://looksfamiliardocdbdev.documents.azure.com:443/"
-$docdbkey = "iW1zbOIvIBjkNofYKNqJs3vrC3+PENefjQU0E9/4mHF9vOWYZ8wnvt5bXbVVTa4SEytKxXBIaDtY3V3TUEOg6A=="
-$docdbconnstr = "AccountEndpoint=https://looksfamiliardocdbdev.documents.azure.com:443/;AccountKey=iW1zbOIvIBjkNofYKNqJs3vrC3+PENefjQU0E9/4mHF9vOWYZ8wnvt5bXbVVTa4SEytKxXBIaDtY3V3TUEOg6A==;"
-$redisuri = "looksfamiliarredisdev.redis.cache.windows.net,ssl=true,password=AvUf2iyH1u6NNGTRYn+6xF3SnT5vNKdUC5kPCSAW12w="
+$docdburi = "[DocumentDb URI]"
+$docdbkey = "[DocumentDb Key]"
+$docdbconnstr = "[DocumentDb Connection String]"
+$redisuri = "[redis-account].redis.cache.windows.net,ssl=true,password=[redis-key]"
